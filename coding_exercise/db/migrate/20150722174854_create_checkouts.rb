6a6a5ef8209775861bec2b82117e79f37725cbe7
Aldo Delgado <aldo@osstek.com>
2015-07-22 14:53:41 -0400

    Added fields to migration for checkout model

--- coding_exercise/db/migrate/20150722174854_create_checkouts.rb
+++ coding_exercise/db/migrate/20150722174854_create_checkouts.rb
@@ -1,7 +1,7 @@
 class CreateCheckouts < ActiveRecord::Migration
   def change
     create_table :checkouts do |t|
-
+      t.decimal :total, precision: 10, scale: 2
       t.timestamps null: false
     end
   end
