870c929498e84179b94082a89cccfcdfe0597b29
Aldo Delgado <aldo@osstek.com>
2015-07-22 13:44:49 -0400

    Modifed generated products migration

--- coding_exercise/db/migrate/20150722173908_create_products.rb
+++ coding_exercise/db/migrate/20150722173908_create_products.rb
@@ -1,7 +1,9 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :code, default: ''
+      t.string :name, default: ''
+      t.decimal :price, precision: 10, scale: 2
       t.timestamps null: false
     end
   end
