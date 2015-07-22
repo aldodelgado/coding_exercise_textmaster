390b92629c6228de7f8a0c6b4262a4f7fce514fb
Aldo Delgado <aldo@osstek.com>
2015-07-22 14:24:55 -0400

    Added rspec model testing for product

--- coding_exercise/test/models/product_test.rb
+++ coding_exercise/test/models/product_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save article without code" do
+    product = Product.new
+    product.name = 'Fruit tea'
+    product.price = 3.11
+    assert_not product.save
+  end
+
+  test "should not save article without name" do
+    product = Product.new
+    product.code = 'FR1'
+    product.price = 3.11
+    assert_not product.save
+  end
+
+  test "should not save article without price" do
+    product = Product.new
+    product.code = 'FR1'
+    product.name = 'Fruit tea'
+    assert_not product.save
+  end
+
+  test "should not negative price" do
+    product = Product.new
+    product.code = 'FR1'
+    product.name = 'Fruit tea'
+    product.price = -3.11
+    assert_not product.save
+  end
+
 end
