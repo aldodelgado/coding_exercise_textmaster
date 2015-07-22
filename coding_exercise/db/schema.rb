6aeb6fde37dce74f1e0d15720a1bab6a217146ec
Aldo Delgado <aldo@osstek.com>
2015-07-22 15:03:50 -0400

    Ran migrations

--- coding_exercise/db/schema.rb
+++ coding_exercise/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722174854) do
+ActiveRecord::Schema.define(version: 20150722184813) do
+
+  create_table "baskets", force: :cascade do |t|
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "checkouts", force: :cascade do |t|
     t.datetime "created_at", null: false
