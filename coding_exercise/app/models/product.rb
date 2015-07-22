7e70b17681f17a45c9a83aba1bd13958471e9e15
Aldo Delgado <aldo@osstek.com>
2015-07-22 14:05:01 -0400

    Adjusted the validation to include message

--- coding_exercise/app/models/product.rb
+++ coding_exercise/app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
-  validates_presence_of :code, :name, :price
+  validates_presence_of :code, message: 'Must have a Product Code'
+  validates_presence_of :name, message: 'Must have a Product Name'
+  validates_presence_of :price, message: 'Must have a Product Price'
+  validates_numericality_of :price, :greater_than => 0
 end
