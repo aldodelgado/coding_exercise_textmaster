15c409a3eb2e7eb42fa6e2b58eaf4af4f36c9863
Aldo Delgado <aldo@osstek.com>
2015-07-22 20:37:05 -0400

    Fixed seeds file

--- coding_exercise/db/seeds.rb
+++ coding_exercise/db/seeds.rb
@@ -1,17 +1,17 @@
 products = Product.create([
   {
-    code: 'FR1'
-    name: 'Fruit Tea'
+    code: 'FR1',
+    name: 'Fruit Tea',
     price: 3.11
   },
   {
-    code: 'AP1'
-    name: 'Apple'
+    code: 'AP1',
+    name: 'Apple',
     price: 5.00
   },
   {
-    code: 'CF1'
-    name: 'Coffee'
+    code: 'CF1',
+    name: 'Coffee',
     price: 11.23
   },
 ])
