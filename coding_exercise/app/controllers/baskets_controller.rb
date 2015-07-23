4962af62040afaec97e005ce3022fcad5baad534
Aldo Delgado <aldo@osstek.com>
2015-07-23 02:42:30 -0400

    Removed basket since I decided to use angularjs to show case angular

--- coding_exercise/app/controllers/baskets_controller.rb
+++
@@ -1,2 +0,0 @@
-class BasketsController < ApplicationController
-end
--- coding_exercise/db/migrate/20150722184813_create_baskets.rb
+++
@@ -1,8 +0,0 @@
-class CreateBaskets < ActiveRecord::Migration
-  def change
-    create_table :baskets do |t|
-      t.integer :product_id
-      t.timestamps null: false
-    end
-  end
-end
