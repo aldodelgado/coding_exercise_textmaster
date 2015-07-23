5fce6d5f076fe24e39548ade97da1e3e6d65c8d7
Aldo Delgado <aldo@osstek.com>
2015-07-23 19:48:03 -0400

    WIP working on checkout logic

--- coding_exercise/app/controllers/checkouts_controller.rb
+++ coding_exercise/app/controllers/checkouts_controller.rb
@@ -1,6 +1,30 @@
 class CheckoutsController < ApplicationController
+  expose(:checkouts){ Checkout.all }
+  expose(:checkout, attributes: :checkout_params)
+  layout nil
 
-  def add
+  def create
+
+    co = Checkout.new(pricing_rules)
+    params[:data][:items].each do |item|
+      co.scan(item)
+    end
     debugger
+
+    redirect_to products_path
+  end
+
+  private
+
+  def pricing_rules
+    pricing_rule = { rule: 'bogo' }
+    #pricing_rule = { rule: 'bulk' }
+    pricing_rule
+  end
+
+  def checkout_params
+    params.require(:checkout).permit(
+      :total
+    )
   end
 end
--- coding_exercise/app/models/checkout.rb
+++ coding_exercise/app/models/checkout.rb
@@ -1,8 +1,19 @@
 class Checkout < ActiveRecord::Base
 
-  def scan(product)
-    puts 'moo'
-    puts 'moo'
+  attr_accessor :rule
+  attr_accessor :running_total
+
+  def scan(item)
+    if self.rule == 'bogo'
+      discount = item[:quantity].to_f / 2
+      cost = discount.to_i * item[:price].to_f
+      unless discount % 1 == 0
+        cost = cost + item[:price].to_f
+      end
+      if se
+      self.running_total = cost
+      debugger
+    end
   end
 
   def total
