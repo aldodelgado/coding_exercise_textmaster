b084b4af10ec3679387f2f3094351313486a14e0
Aldo Delgado <aldo@osstek.com>
2015-07-23 03:14:52 -0400

    Added expose to products controller

--- coding_exercise/app/controllers/products_controller.rb
+++ coding_exercise/app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+  expose(:products){ Product.all}
+  expose(:product, attributes: :product_params)
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :code,
+      :name,
+      :price
+    )
+  end
 end
