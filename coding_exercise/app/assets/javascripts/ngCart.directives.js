d7266b3e7e333e628901b786b51da597a0ef69af
Aldo Delgado <aldo@osstek.com>
2015-07-23 03:13:51 -0400

    Added ngCart library

---
+++ coding_exercise/app/assets/javascripts/ngCart.directives.js
@@ -0,0 +1,111 @@
+'use strict';
+
+angular.module('ngCart.directives', ['ngCart.fulfilment'])
+
+    .controller('CartController',['$scope', 'ngCart', function($scope, ngCart) {
+        $scope.ngCart = ngCart;
+    }])
+
+    .directive('ngcartAddtocart', ['ngCart', function(ngCart){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {
+                id:'@',
+                name:'@',
+                quantity:'@',
+                quantityMax:'@',
+                price:'@',
+                data:'='
+            },
+            transclude: true,
+            templateUrl: function(element, attrs) {
+                if ( typeof attrs.templateUrl == 'undefined' ) {
+                    return '/assets/addtocart.html';
+                } else {
+                    return attrs.templateUrl;
+                }
+            },
+            link:function(scope, element, attrs){
+                scope.attrs = attrs;
+                scope.inCart = function(){
+                    return  ngCart.getItemById(attrs.id);
+                };
+
+                if (scope.inCart()){
+                    scope.q = ngCart.getItemById(attrs.id).getQuantity();
+                } else {
+                    scope.q = parseInt(scope.quantity);
+                }
+
+                scope.qtyOpt =  [];
+                for (var i = 1; i <= scope.quantityMax; i++) {
+                    scope.qtyOpt.push(i);
+                }
+
+            }
+
+        };
+    }])
+
+    .directive('ngcartCart', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            templateUrl: function(element, attrs) {
+                if ( typeof attrs.templateUrl == 'undefined' ) {
+                    return '/assets/cart.html';
+                } else {
+                    return attrs.templateUrl;
+                }
+            },
+            link:function(scope, element, attrs){
+
+            }
+        };
+    }])
+
+    .directive('ngcartSummary', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            transclude: true,
+            templateUrl: function(element, attrs) {
+                if ( typeof attrs.templateUrl == 'undefined' ) {
+                    return '/assets/summary.html';
+                } else {
+                    return attrs.templateUrl;
+                }
+            }
+        };
+    }])
+
+    .directive('ngcartCheckout', [function(){
+        return {
+            restrict : 'E',
+            controller : ('CartController', ['$scope', 'ngCart', 'fulfilmentProvider', function($scope, ngCart, fulfilmentProvider) {
+                $scope.ngCart = ngCart;
+
+                $scope.checkout = function () {
+                    fulfilmentProvider.setService($scope.service);
+                    fulfilmentProvider.setSettings($scope.settings);
+                    var promise = fulfilmentProvider.checkout();
+                    console.log(promise);
+                }
+            }]),
+            scope: {
+                service:'@',
+                settings:'='
+            },
+            transclude: true,
+            templateUrl: function(element, attrs) {
+                if ( typeof attrs.templateUrl == 'undefined' ) {
+                    return '/assets/checkout.html';
+                } else {
+                    return attrs.templateUrl;
+                }
+            }
+        };
+    }]);
---
+++ coding_exercise/app/assets/javascripts/ngCart.fulfilment.coffee
@@ -0,0 +1,47 @@
+angular.module('ngCart.fulfilment', []).service('fulfilmentProvider', [
+  '$injector'
+  ($injector) ->
+    @_obj =
+      service: undefined
+      settings: undefined
+
+    @setService = (service) ->
+      @_obj.service = service
+      return
+
+    @setSettings = (settings) ->
+      @_obj.settings = settings
+      return
+
+    @checkout = ->
+      provider = $injector.get('ngCart.fulfilment.' + @_obj.service)
+      provider.checkout @_obj.settings
+
+    return
+]).service('ngCart.fulfilment.log', [
+  '$q'
+  '$log'
+  'ngCart'
+  ($q, $log, ngCart) ->
+
+    @checkout = ->
+      deferred = $q.defer()
+      $log.info ngCart.toObject()
+      deferred.resolve cart: ngCart.toObject()
+      deferred.promise
+
+    return
+]).service('ngCart.fulfilment.http', [
+  '$http'
+  'ngCart'
+  ($http, ngCart) ->
+
+    @checkout = (settings) ->
+      $http.post settings.url, data: ngCart.toObject()
+
+    return
+]).service 'ngCart.fulfilment.paypal', [
+  '$http'
+  'ngCart'
+  ($http, ngCart) ->
+]
